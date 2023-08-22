4f68fa4392e4cc36e6a9a2f17b774ed0567d86f5
Sagar <sagar.0dev@gmail.com>
2023-08-22 17:31:32 +0530

    main

--- app/src/main/java/com/example/animations/MainActivity.kt
+++ app/src/main/java/com/example/animations/MainActivity.kt
@@ -3,20 +3,7 @@ package com.example.animations
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
-import androidx.compose.animation.graphics.res.animatedVectorResource
-import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
-import androidx.compose.animation.graphics.vector.AnimatedImageVector
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.animations.ui.theme.AnimationsTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,25 +11,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AnimationsTheme {
-                AnimatedVectorDrawable()
+                CheckOtherBranches()
             }
         }
     }
 }
 
-@OptIn(ExperimentalAnimationGraphicsApi::class)
-@Preview
 @Composable
-fun AnimatedVectorDrawable() {
-    val image = AnimatedImageVector
-        .animatedVectorResource(R.drawable.ic_hourglass_animated)
-    var atEnd by remember { mutableStateOf(false) }
-    Image(
-        painter = rememberAnimatedVectorPainter(image, atEnd),
-        contentDescription = "Timer",
-        modifier = Modifier.clickable {
-            atEnd = !atEnd
-        },
-        contentScale = ContentScale.Crop
-    )
+fun CheckOtherBranches() {
+
 }
