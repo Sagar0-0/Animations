13ff66ba9c2baaf086e420954d49c6f83eeae331
Sagar <sagar.0dev@gmail.com>
2023-08-22 18:23:10 +0530

    InfiniteTransition

--- app/src/main/java/com/example/animations/MainActivity.kt
+++ app/src/main/java/com/example/animations/MainActivity.kt
@@ -3,7 +3,20 @@ package com.example.animations
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.animations.ui.theme.AnimationsTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,13 +24,28 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AnimationsTheme {
-                CheckOtherBranches()
+                InfiniteTransition()
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun CheckOtherBranches() {
+fun InfiniteTransition() {
+    val infiniteTransition = rememberInfiniteTransition(label = "")
+    val color by infiniteTransition.animateColor(
+        initialValue = Color.Red,
+        targetValue = Color.Green,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        ), label = ""
+    )
 
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color)
+    )
 }
