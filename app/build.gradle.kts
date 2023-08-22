a7ce10bb61fbd2f3bdf3c0a9702812601c18405d
Sagar <sagar.0dev@gmail.com>
2023-08-22 17:27:42 +0530

    lottieAnimation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.animation:animation-graphics-android:1.5.0")
+
+    implementation("com.airbnb.android:lottie-compose:6.1.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/example/animations/MainActivity.kt
+++ app/src/main/java/com/example/animations/MainActivity.kt
@@ -3,20 +3,12 @@ package com.example.animations
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
-import androidx.compose.animation.graphics.res.animatedVectorResource
-import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
-import androidx.compose.animation.graphics.vector.AnimatedImageVector
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.example.animations.ui.theme.AnimationsTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,25 +16,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AnimationsTheme {
-                AnimatedVectorDrawable()
+                Loader()
             }
         }
     }
 }
 
-@OptIn(ExperimentalAnimationGraphicsApi::class)
 @Preview
 @Composable
-fun AnimatedVectorDrawable() {
-    val image = AnimatedImageVector
-        .animatedVectorResource(R.drawable.ic_hourglass_animated)
-    var atEnd by remember { mutableStateOf(false) }
-    Image(
-        painter = rememberAnimatedVectorPainter(image, atEnd),
-        contentDescription = "Timer",
-        modifier = Modifier.clickable {
-            atEnd = !atEnd
-        },
-        contentScale = ContentScale.Crop
+fun Loader() {
+    val composition by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(R.raw.loading)
     )
+    LottieAnimation(composition)
 }
